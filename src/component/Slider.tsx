d53cfa48a70945d7916efef846f1d222d21a12a7
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2024-10-16 02:16:09 +0330

    slider timer

--- src/component/Slider.tsx
+++ src/component/Slider.tsx
@@ -1,6 +1,5 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
@@ -8,9 +7,11 @@ import "swiper/css/pagination";
 import "../index.css";
 
 // import required modules
-import { Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
+import SwiperCore from "swiper";
 
 export default function Slider() {
+  SwiperCore.use([Autoplay]);
   const src = [
     { id: "1", src: "slider (1).gif" },
     { id: "2", src: "slider (1).jpg" },
@@ -26,22 +27,18 @@ export default function Slider() {
   return (
     <>
       <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
         pagination={{
           dynamicBullets: true,
         }}
         modules={[Pagination]}
         className="mySwiper"
       >
-        {/* <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide> */}
         {src.map((item) => {
           return (
             <SwiperSlide key={item.id}>
